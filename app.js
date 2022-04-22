9b0feea6bf7d736f5d007df3623b5938a8a5c9c7
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2022-04-22 11:46:20 -0500

    bug fixing on heroku deployment related to port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT | 3000;
 
 const loginRoutes = require('./routes/login');
 
@@ -8,4 +10,4 @@ app.use(express.json());
 
 app.use('/api', loginRoutes);
 
-app.listen(3000);
+app.listen(port);
